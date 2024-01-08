2d7cfe54c6443d6412c1a4923e47e4c1e2bd6fee
Michael Owen <m.harry.owen@gmail.com>
2024-01-08 16:46:31 -0500

    scaffold contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,25 +3,102 @@
 </script>
 
 <template>
-  <section class="section-contact">
-    <h1>Contact</h1>
+  <div class="page-contact">
+    <h1>Get in Touch</h1>
 
     <div class="contact">
-      <p>
-        Thanks for stopping by! This page is under construction.
+      <p class="contact-msg">
+        I'm so glad to hear you're interested in learning more! Just fill out and submit this form,
+        and I'll get back to you inside 24–48 hours. I look forward to hearing from you!
       </p>
+
+      <form class="form">
+        <div class="form-input inline">
+          <label for="name" class="label">Your Name</label>
+          <input id="name" type="text" placeholder="What should I call you?" class="input text-center" required />
+        </div>
+        <div class="form-input inline">
+          <label for="email" class="label">Your Email</label>
+          <input id="email" type="email" placeholder="you@site.com" class="input text-center" required />
+        </div>
+        <div class="form-input inline">
+          <label for="phone" class="label">Daytime Phone (optional)</label>
+          <input id="phone" type="tel" placeholder="+1 (123) 555-5555" class="input text-center" />
+        </div>
+        
+        <div class="form-input inline">
+          <label for="service_select" class="label">Service of Interest</label>
+          <select id="service_select" name="service" class="input text-center">
+            <option>--Choose an Option--</option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+            <option>Not sure -- details below!</option>
+          </select>
+        </div>
+        <div class="form-input">
+          <label for="details" class="label-long">
+            In as many words as you like, tell me more about
+            <span class="c-accent fw-bold">what I can do for you.</span>
+            Feel free to include any links or resources you'd like for me to look at!
+          </label>
+          <textarea
+            id="details"
+            name="details"
+            class="input"
+            rows="6"
+            placeholder="Your remit here..."
+            required></textarea>
+        </div>
+        <button
+          @click.prevent
+          class="btn btn-accent">Submit</button>        
+      </form>
     </div>
-  </section>
+  </div>
 </template>
 
 <style scoped>
-.section-contact {
-  margin-inline: auto;
-  padding-inline: 1rem;
-  width: min(var(--dw-content-wide), calc(100% - 1rem));
+.page-contact {
+  --form-max-inline: 50rem;
+  --leftover: calc((100vw - var(--dw-content-wide)) / 2);
+  display: grid;
+  align-items: start;
+  grid-template-rows: auto 1fr;
+  padding-inline: max(calc(var(--leftover) + 1rem), 1rem);
+  /* inline padding >=1rem plus however much is needed to align flush with header */
 }
 .contact {
+  display: grid;
   font-size: var(--step-1);
-  margin-top: 5rem;
+  gap: 2rem;
+  margin-block: 2rem;
+  max-width: var(--form-max-inline);
 }
+.form {
+  display: flex;
+  flex-wrap: wrap;
+  font-size: var(--step-0);
+  gap: 2rem 1rem;
+}
+.form-input {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  text-align: center;
+}
+.form-input.inline {
+  flex-direction: column-reverse;
+  flex: 1 0 20rem;
+}
+.label { color: var(--c-accent) }
+.label-long { text-align: start }
+.input {
+  appearance: none;
+  background-color: var(--c-secondary);
+  color: var(--c-text);
+  padding: 0.5rem 1rem;
+}
+.input::placeholder { font-style: italic }
+.form > .btn { flex-grow: 1 }
 </style>
