fd21c9f73f0e5a2f408e7bb3082471a9c75344cf
mhowen <m.harry.owen@gmail.com>
2024-01-29 13:59:33 -0500

    split complex dynamic textarea into discrete component

---
+++ components/DynamicTextarea.vue
@@ -0,0 +1,58 @@
+<script setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  rows: {
+    type: Number,
+    default: 6,
+  },
+  placeholder: {
+    type: String,
+    default: 'Your response here...',
+  },
+});
+
+const inputValue = defineModel();
+</script>
+
+<template>
+  <div class="dtextarea">
+    <label for="dta">
+      <slot />
+    </label>
+    <textarea
+      id="dta"
+      v-model="inputValue"
+      :placeholder="placeholder"
+      :rows="rows"
+    >
+    </textarea>
+  </div>
+</template>
+
+<style scoped>
+.dtextarea {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+#dta {
+  appearance: none;
+  background-color: var(--c-secondary);
+  color: var(--c-text);
+  padding: 0.5rem 1rem;
+}
+#dta[disabled] {
+  filter: grayscale();
+  opacity: 0.25;
+}
+#dta::placeholder {
+  font-style: italic;
+}
+</style>
