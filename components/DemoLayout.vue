a5db2bab2d6a46d8358ed8eada01dd04fe02371e
mhowen <m.harry.owen@gmail.com>
2024-02-08 10:55:51 -0500

    polish layout demo to remotely presentable initial level

--- components/DemoLayout.vue
+++ components/DemoLayout.vue
@@ -9,10 +9,10 @@ defineProps({
 
 <template>
   <div class="demolayout" :class="layout">
-    <Transition name="layoutfade" mode="out-in">
+    <!-- <Transition name="layoutfade" mode="out-in"> -->
       <div class="blog" v-if="layout === 'blog'">
         <h1 class="blog-heading">Blog</h1>
-        <div class="blog-info">
+        <div class="blog-info bg-alt">
           <h3>About</h3>
           <ul>
             <li>Thing 1</li>
@@ -20,7 +20,7 @@ defineProps({
             <li>Thing 3</li>
           </ul>
         </div>
-        <div class="blog-body">
+        <div class="blog-body bg-main">
           <h2>Eye-Catching Title!</h2>
           <p>
             Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
@@ -35,7 +35,7 @@ defineProps({
             ullamco ut ea consectetur et est culpa et culpa duis.
           </p>
         </div>
-        <div class="blog-comments">
+        <div class="blog-comments bg-alt">
           <h3>Discuss</h3>
           <ul>
             <li>Thing 1</li>
@@ -51,7 +51,7 @@ defineProps({
       <div class="store" v-else-if="layout === 'store'">
         <h1 class="store-heading">Store</h1>
         <div class="store-main">
-          <div class="sidebar">
+          <div class="sidebar bg-alt">
             <h3>Shopping Cart</h3>
             <ul>
               <li>Item 1</li>
@@ -61,11 +61,12 @@ defineProps({
           </div>
           <div class="items">
             <ul>
-              <li v-for="i in 15" :key="'item' + i" class="item">
-                <p>Item Image</p>
-                <p>Item Desc.</p>
-                <p> ***** </p>
+              <li v-for="i in 15" :key="'item' + i" class="item bg-main">
+                <p class="italic">Item Description</p>
                 <p> $0.01 </p>
+                <p>
+                  &#9733;&#9733;&#9733;&#9733;{{ Math.random() > 0.5 ? '\u2605' : '\u2606' }}
+                </p>
               </li>
             </ul>
           </div>
@@ -75,16 +76,16 @@ defineProps({
       <div class="home" v-else>
         <h1 class="home-heading fw-black uppercase">Company</h1>
         <p class="home-tagline">
-          <span class="fw-bold uppercase">Lorem</span>
+          <span class="c-alt fw-bold uppercase">Lorem</span>
           <span class="fw-thin italic"> made </span>
-          <span class="fw-bold uppercase">Ipsum</span>
+          <span class="c-alt fw-bold uppercase">Ipsum</span>
         </p>
         <div class="home-buttons">
-          <button class="fw-bold uppercase">Try Now</button>
+          <button class="bg-main c-white fw-bold uppercase">Try Now</button>
           <button class="uppercase">Learn More</button>
         </div>
       </div>
-    </Transition>
+    <!-- </Transition> -->
   </div>
 </template>
 
@@ -98,12 +99,18 @@ defineProps({
   opacity: 0;
 }
 .demolayout {
+  --c-demo: hsl(172 50% 39% / .3);
+  --c-demo-alt: hsl(30 40% 51% / .3);
   container: cnt-demolayout / inline-size;
   background-color: #404040;
   min-height: 100%;
   max-height: 1000px;
   padding-inline: 1rem;
 }
+.demolayout .bg-main { background-color: var(--c-demo) }
+.demolayout .bg-alt { background-color: var(--c-demo-alt) }
+.demolayout .c-main { color: var(--c-demo) }
+.demolayout .c-alt { color: hsl(30 30% 51%) }
 .demolayout.home {
   display: grid;
   place-content: center;
@@ -149,6 +156,7 @@ defineProps({
 .sidebar {
   border: 0.125rem solid #141414;
   flex: 1 1 25rem;
+  padding: 1rem;
 }
 .items {
   flex: 999 1 0;
@@ -164,12 +172,35 @@ defineProps({
 .item {
   aspect-ratio: 1;
   border: 0.125rem solid #141414;
+  display: grid;
+  align-items: end;
+  gap: 1rem;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr auto;
   padding: 1rem;
+  position: relative;
+}
+.item > :first-child {
+  grid-column: span 2;
+}
+.item > :last-child {
+  justify-self: end;
+}
+.item::before {
+  content: 'Item Image';
+  display: grid;
+  place-items: center;
+  font-size: 3rem;
+  inset: 0;
+  opacity: 0.25;
+  position: absolute;
+  z-index: -1;
 }
 .blog {
   flex: 1;
   font-size: clamp(1rem, 4cqw + 0.125rem, 1.5rem);
   display: grid;
+  gap: 1rem;
   grid-template-areas: "head" "bar1" "body" "bar2" "foot";
   grid-template-rows: auto auto 1fr auto auto;
 }
@@ -185,21 +216,19 @@ defineProps({
 .blog-info {
   grid-area: bar1;
   border: 0.125rem solid #141414;
-  border-bottom: none;
 }
 .blog-body {
   grid-area: body;
-  border-top: 0.125rem solid #141414;
+  border: 0.125rem solid #141414;
 }
 .blog-body > p { max-width: 60ch }
 .blog-comments {
   grid-area: bar2;
   border: 0.125rem solid #141414;
-  border-bottom: none;
 }
 .blog-footer {
   grid-area: foot;
-  border-top: 0.125rem solid #141414;
+  border: 0.125rem solid #141414;
 }
 @container cnt-demolayout (min-width: 40rem) {
   .blog {
