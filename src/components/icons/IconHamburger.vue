965686a1c3acd55041585cb514736304281cd018
mhowen <m.harry.owen@gmail.com>
2024-05-29 16:40:46 -0400

    fix: icon missing accessible description

--- src/components/icons/IconHamburger.vue
+++ src/components/icons/IconHamburger.vue
@@ -1,5 +1,6 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+    <desc>A hamburger icon representing an expandable navigation menu</desc>
     <path fill="currentColor" d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z"/>
   </svg>
 </template>
